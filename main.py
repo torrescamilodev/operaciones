5711ef794fb5bd9e849c4e8e8b08bab5de30bcb0
Camilo Torres <torrescamilodev@gmail.com>
2024-02-26 23:51:50 -0500

    solved

--- main.py
+++ main.py
@@ -16,4 +16,19 @@ if option not in [1,2,3]:
     print ("Valor no valido")
     exit()
 
+if option == 1:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print("Resultado:", suma(num1, num2))
+elif option == 2:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print("Resultado:", resta(num1, num2))
+elif option == 3:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    if num2 != 0:
+        print("Resultado:", division(num1, num2))
+    else:
+        print("No se puede dividir entre cero")
 
